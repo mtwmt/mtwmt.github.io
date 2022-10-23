e2302e20e71f57870c8d7b0bc1339d72d766582b
Mandy <mtwmt@hotmail.com>
2022-10-23 20:37:34 +0800

    feat(website): update

--- src/app/shared/services/highlight.service.ts
+++ src/app/shared/services/highlight.service.ts
@@ -7,6 +7,7 @@ import 'prismjs/plugins/toolbar/prism-toolbar';
 import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
 import 'prismjs/components/prism-bash';
 import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-scss';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-markup';
