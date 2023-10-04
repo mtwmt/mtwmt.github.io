6da15903facf6ad17e68c2ccaa41eb36b9dc8077
Mandy <mtwmt@hotmail.com>
2023-10-05 02:25:46 +0800

    update

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,20 +1,19 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function get(context) {
-	const posts = await getCollection('blog');
-	return rss({
+  const posts = await getCollection("blog");
+  return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    // items: []
     items: posts.map((post) => ({
-    	// ...post.data,
-	title: post.data?.title || '',
-        pubDate: post.data?.update || post.data?.date,
-	description: '',
-    	link: `/blog/${post.slug}/` || '',
+      // ...post.data,
+      title: post.data?.title || "",
+      pubDate: post.data?.update || post.data?.date,
+      description: "",
+      link: `/blog/${post.slug}/` || "",
     })),
   });
 }
